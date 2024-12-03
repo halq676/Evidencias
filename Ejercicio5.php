9e48c4743c6dc9d693ff1fc9c65cd073fbbf3dc1
halq676 <henlombana06@gmail.com>
2024-12-03 14:28:54 -0500

    Add files via upload

---
+++ Ejercicio5.php
@@ -0,0 +1,10 @@
+<?php
+// Sumar los números del 1 al 10
+$suma = 0;
+
+for ($i = 1; $i <= 10; $i++) {
+    $suma += $i; // Agrega cada número a la suma
+}
+
+echo "La suma de los números del 1 al 10 es: " . $suma . "\n";
+?>
