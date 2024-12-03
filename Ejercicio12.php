d1c05ecccdc6cfceb06794731ab10d2c4b1a4b4f
halq676 <henlombana06@gmail.com>
2024-12-03 14:35:11 -0500

    Add files via upload

---
+++ Ejercicio12.php
@@ -0,0 +1,8 @@
+<?php
+$nombre = "Alfonso";
+$edad = 55;
+$mensaje = sprintf("Mi nombre es %s y tengo %d años.", $nombre, $edad);
+echo $mensaje;  // Imprime: Mi nombre es Alfonso y tengo 55 años.
+
+
+?>
