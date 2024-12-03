86ebdb6e8c3cadf70339eacc06c9bddadc6d2e1d
halq676 <henlombana06@gmail.com>
2024-12-03 14:34:04 -0500

    Add files via upload

---
+++ Ejercicio11.php
@@ -0,0 +1,6 @@
+<?php
+$cadena = "Buenos dias, Profesor!";
+$cadena_modificada = str_replace("Profesor", "Tutor", $cadena); // Remplaza profesor por tutor
+echo $cadena_modificada;  // Imprime: Buenos dias, Tutor!
+
+?>
