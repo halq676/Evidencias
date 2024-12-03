3043af3e64d68184253d3bf58e341b9ee02783cb
halq676 <henlombana06@gmail.com>
2024-12-03 14:24:36 -0500

    Add files via upload
    Condicional if

---
+++ Ejercicio2.php
@@ -0,0 +1,22 @@
+
+<html>
+<head>
+<title>Ejercicios evidencias</title>
+
+</head>
+
+<body>
+<?php
+$a = 8;
+$b = 3;
+if ($a < $b)
+{
+echo "a es menor que b";
+}
+else
+{
+echo "a no es menor que b";
+}
+?>
+</body>
+</html>
