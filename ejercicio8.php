d6cdb8a0239bcfa9a086a433a5867654d58720c2
halq676 <henlombana06@gmail.com>
2024-12-03 14:31:40 -0500

    Add files via upload

---
+++ ejercicio8.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+<title>Ejercicio evidencias</title>
+</head>
+<body>
+Inicio<BR>
+<?php
+$i=0;
+while ($i<10) //condicion
+{
+echo "El valor de i es ", $i,"<br>";
+$i++;
+}
+?>
+Final<BR>
+</body>
+</html>
