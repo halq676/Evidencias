ac5dac0a94be3ad42962608c52d5c8b512031c93
halq676 <henlombana06@gmail.com>
2024-12-03 14:28:21 -0500

    Add files via upload

---
+++ Ejercicio4.php
@@ -0,0 +1,11 @@
+<?php
+//Sentencia if ejecuta condicion verdaera
+//else ejecuta la condicion false
+    $edad = 18;
+
+    if ($edad >= 18) {
+        echo "Eres mayor de edad.";
+    } else {
+        echo "Eres menor de edad.";
+    }
+?>
